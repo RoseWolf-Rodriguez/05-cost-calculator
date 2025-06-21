e8664d50494ed0ea935134edaeb5209c66db1da0
Lucretia Teretto <lteretto@asu.edu>
2025-06-21 13:52:55 +0000

    following along with Guil

---
+++ script.js
@@ -0,0 +1,13 @@
+function addNumbers (a, b) {
+  return a + b;
+}
+
+function multiplyNumbers(a, b) {
+  return a * b;
+}
+
+console.log(multiplyNumbers(2, 3))
+console.log(multiplyNumbers(4, 5))
+
+console.log(addNumbers(5, 3)); 
+console.log(addNumbers(10, 20));
